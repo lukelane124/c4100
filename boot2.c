9889e52049efa1bd66dd303828e53ce0d7d50fd6
coursework <coursework>
2017-03-21 01:39:22 +0000

    This commit is to move changes to keyboard handlers into repo. at this time things are written to screen, but they are not the correct key, and always the same...

--- boot2.c
+++ boot2.c
@@ -1,22 +1,17 @@
 //boot2.c
 //Declare a struct with 64 bits... Might work, might not.
 #include<stdint.h>
+#define true 1
+#define false 0
+//Structure declaration section
 struct IDT_s {
 	uint16_t base_low;
 	uint16_t selector;
 	uint8_t always0;
 	uint8_t access;
 	uint16_t base_high;
-	
-  // unsigned int base_high : 16;
-  // unsigned int access : 8;
-  // unsigned int selector : 8;
-  // unsigned int base_low : 16;
 } __attribute__((packed));
 typedef struct IDT_s IDT;
-
-void default_handler();
-void kdb_enter();
 
 struct gdt_r_s {
 	uint16_t limit;
@@ -24,8 +19,143 @@ struct gdt_r_s {
 } __attribute__((packed));
 typedef struct gdt_r_s gdt_r_t;
 
+//CircularQueue, and associated Functions.
+#define MAX_ITEMS 128
+struct keyboardBuffer_s {
+    short top;
+    short tail;
+    short numItems;
+    short mxItems;
+    char data[MAX_ITEMS];
+} __attribute__((packed));
+typedef struct keyboardBuffer_s keyboardBuffer_t;
+void initializeQueue(keyboardBuffer_t *q) {
+	int i;
+	q->top = 0;
+	q->tail = 0;
+	q->numItems = 0;
+	q->mxItems = MAX_ITEMS ;
+	for (i = 0; i < MAX_ITEMS; i++) {
+		q->data[i] = 0;
+	}
+	return;
+}
+
+char isEmpty(keyboardBuffer_t *q) {
+	if ( q->numItems == 0)
+		return true;
+	else
+		return false;
+}
+
+char putItem(keyboardBuffer_t *q, unsigned char theItemValue) {
+	if (q->numItems >= MAX_ITEMS)
+		return false;
+	else {
+		q->data[q->tail++] = theItemValue;
+		if (q->tail > MAX_ITEMS)
+			q->tail = 0;
+		q->numItems++;
+		return true;
+	}
+}
+
+unsigned char getItem(keyboardBuffer_t *q) {
+	char ret;
+	if (isEmpty(q))
+		return 0;
+	else{
+		ret = q->data[q->top++];
+		if (q->top > MAX_ITEMS)
+			q->top = 0;
+		q->numItems--;
+
+	}
+	return ret;	
+}
+
+
+
+enum CSET_1 {
+Q_PRESSED = 0x10, W_PRESSED = 0x11, E_PRESSED = 0x12, R_PRESSED = 0x13,
+T_PRESSED = 0x14, Y_PRESSED = 0x15, U_PRESSED = 0x16, I_PRESSED = 0x17,
+O_PRESSED = 0x18, P_PRESSED = 0x19
+};
+static char* cset_1_chars = "qwertyuiop";
+
+enum CSET_2 {
+A_PRESSED = 0x1E, S_PRESSED = 0x1F, D_PRESSED = 0x20, F_PRESSED = 0x21,
+G_PRESSED = 0x22, H_PRESSED = 0x23, J_PRESSED = 0x24, K_PRESSED = 0x25,
+L_PRESSED = 0x26
+};
+static char *cset_2_chars = "asdfghjkl";
+
+enum CSET_3 {
+Z_PRESSED = 0x2C, X_PRESSED = 0x2D, C_PRESSED = 0x2E, V_PRESSED = 0x2F,
+B_PRESSED = 0x30, N_PRESSED = 0x31, M_PRESSED = 0x32,
+};
+static char *cset_3_chars = "zxcvbnm";
+
+enum CSET_NUMBERS {
+ONE_PRESSED = 0x2, TWO_PRESSED = 0x3, THREE_PRESSED = 0x4,
+FOUR_PRESSED = 0x5, FIVE_PRESSED = 0x6, SIX_PRESSED = 0x7,
+SEVEN_PRESSED = 0x8, EIGHT_PRESSED = 0x9, NINE_PRESSED = 0xA
+};
+
+#define CSET_ZERO 0x0B
+
+#define CSET_NL 0x1C
+#define CSET_SPC 0x39
+#define CSET_RET 0xE
+#define CSET_POINT_PRESSED 0x34
+#define CSET_SLASH_PRESSED 0x35
+
+//Prototype Section
+void noShut();
+void show_eax();
+void clearScr();
+void writeScr(char *string, int row, int col);
+void default_handler();
+void kdb_enter();
+void causeFault();
+char translate_scancode(int what);
+void kbd_handler(unsigned int scancode);
+
+//Global variable section
 IDT idt_entry_t[256];
 int current_idt_entry_count = 0;
+keyboardBuffer_t keyboard_buffer;
+
+//C Functions.
+char translate_scancode(int code) {
+	if (code > 0x9 && code < 0x20)
+		return cset_1_chars[code];
+	if (code > 0x1e && code < 0x27)
+		return cset_2_chars[code];
+	if (code > 0x2b && code < 0x33)
+		return cset_3_chars[code];
+	if (code > 0x1 && code < 0xa)
+		return code - 1;
+	if (code == CSET_ZERO)
+		return '0';
+	if (code = CSET_NL)
+		return '\n';
+	if (code = CSET_SPC)
+		return ' ';
+	if (code == CSET_RET)
+		return 8;
+	if (code == CSET_POINT_PRESSED)
+		return '.';
+	if (CSET_SLASH_PRESSED == code)
+		return '/';
+}
+char k_getchar() {
+	if (isEmpty(&keyboard_buffer))
+		return 0;
+	else {
+		return getItem(&keyboard_buffer);
+	}
+}
 
 void initIDTEntry(IDT *entry, uint32_t base, uint16_t selector, uint8_t access){
 	entry->base_low = (base & 0xffff);
@@ -73,10 +203,6 @@ void setupPIC() {
 }
 
 
-void noShut();
-void show_eax();
-void clearScr();
-void writeScr(char *string, int row, int col);
 //void outportb(unsigned int, unsigned int);
 void updateCursor(unsigned int loc) {
 	//int loc = 5;
@@ -87,12 +213,7 @@ void updateCursor(unsigned int loc) {
 }
 
 
-/*inline unsigned char inportb(unsigned int port)
-{
-   unsigned char ret;
-   asm volatile ("inb %%dx,%%al":"=a" (ret):"d" (port));
-   return ret;
-}*/
+
 int convert_num_h(unsigned int num, char buf[]) {
   if (num == 0) {
     return 0;
@@ -103,8 +224,6 @@ int convert_num_h(unsigned int num, char buf[]) {
   return idx + 1;
 }
 
-
-
 void convert_num(unsigned int num, char buf[]) {
   if (num == 0) {
     buf[0] = '0';
@@ -114,16 +233,37 @@ void convert_num(unsigned int num, char buf[]) {
   }
 }
 
-void causeFault();
+//Main Program
 int main() {
+	char ch;
+	char* str[2];
+	int row = 0;
+	int col = 0;
 	//show_eax();
+	initializeQueue(&keyboard_buffer);
 	clearScr();
 	initIDT();
 	setupPIC();
 	asm("sti");
-	//causeFault();
+	str[1] = '\0';
+	
 	
 
-	while(1) {}
+	while(1) {
+		ch = k_getchar();
+
+		if (ch == 0)
+			continue;
+		else {
+			str[0] = ch;
+			writeScr(str, row++, col++);
+			if (row == 26) {
+				clearScr();
+				row = 0;
+			}
+			if (col == 81)
+				col = 0;
+		}
+	}
 	
 }
